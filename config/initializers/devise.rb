a0b9364f22b746f5cdf5ed909a3a08faa59a2b3b
Pauline Bessoles <52420208+paulinebessoles@users.noreply.github.com>
2022-05-19 17:42:57 +0200

    Switch config to 3 days

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -23,5 +23,5 @@ Devise.setup do |config|
   # able to access the website for two days without confirming their account,
   # access will be blocked just in the third day. Default is 0.days, meaning
   # the user cannot access the website without confirming their account.
-  config.allow_unconfirmed_access_for = 0.days
+  config.allow_unconfirmed_access_for = 3.days
 end

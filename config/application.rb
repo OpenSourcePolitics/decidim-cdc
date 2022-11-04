5165fc060fd14dcdf5962f2724f36c292ec0937e
Quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-11-04 10:42:07 +0100

    Bump config application.rb

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,6 @@ require_relative "boot"
 
 require "rails/all"
 
-# TODO : add missing dep to decidim-initiatives/lib/decidim/initiatives/engine.rb
-# require "wicked_pdf"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -14,7 +11,7 @@ Bundler.require(*Rails.groups)
 module DevelopmentApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
     config.time_zone = "Europe/Paris"
     config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml").to_s]
 

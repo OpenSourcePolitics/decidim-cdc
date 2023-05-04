27f5caf84957a45a9aad85e90a60226f7633557a
Elie Gaboriau <elie@opensourcepolitics.eu>
2023-05-04 16:24:46 +0200

    refactof task file

--- spec/lib/tasks/create_admin_tasks_spec.rb
+++ spec/lib/tasks/create_admin_tasks_spec.rb
@@ -5,31 +5,35 @@ require "spec_helper"
 describe "rake decidim_app", type: :task do
   describe "create_system_admin" do
 
+    let!(:task_name) { "decidim_app:create_system_admin" }
+
     before do
       allow(Decidim::SystemAdminCreator).to receive(:create!).with(ENV).and_return(true)
     end
 
     it "preloads the Rails environment" do
-      expect(task.prerequisites).to include "environment"
+      expect(Rake::Task[task_name].prerequisites).to include "environment"
     end
 
     it "invokes the admin creator" do
-      expect { task.invoke }.to output("System admin created successfully\n").to_stdout
+      expect { Rake::Task[task_name].invoke }.to output("System admin created successfully\n").to_stdout
     end
   end
 
   describe "create_admin", type: :task do
+
+    let!(:task_name) { "decidim_app:create_admin" }
 
     before do
       allow(Decidim::AdminCreator).to receive(:create!).with(ENV).and_return(true)
     end
 
     it "preloads the Rails environment" do
-      expect(task.prerequisites).to include "environment"
+      expect(Rake::Task[task_name].prerequisites).to include "environment"
     end
 
     it "invokes the admin creator" do
-      expect { task.invoke }.to output("Admin created successfully\n").to_stdout
+      expect { Rake::Task[task_name].invoke }.to output("Admin created successfully\n").to_stdout
     end
   end
 end

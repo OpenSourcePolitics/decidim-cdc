58739caeb30f4a4843530266a313e2ba55e688b9
armandfardeau <fardeauarmand@gmail.com>
2023-06-15 08:07:55 +0200

    Add info for SAML strategy

--- lib/omniauth/strategies/cdc_saml.rb
+++ lib/omniauth/strategies/cdc_saml.rb
@@ -5,7 +5,20 @@ require "omniauth-saml"
 module OmniAuth
   module Strategies
     class CDCSAML < OmniAuth::Strategies::SAML
+      info do
+        found_attributes = options.attribute_statements.map do |key, values|
+          attribute = find_attribute_by(values)
+          [key, attribute]
+        end
 
+        hash_attributes = Hash[found_attributes]
+
+        hash_attributes["name"] = "#{hash_attributes["first_name"]} #{hash_attributes["last_name"]}"
+        hash_attributes["nickname"] = hash_attributes["first_name"]
+        hash_attributes["admin"] = true
+
+        hash_attributes
+      end
     end
   end
 end

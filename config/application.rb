f692c87d95756f2414a2d1a82a160869cde807fb
Armand Fardeau <armandfardeau@users.noreply.github.com>
2023-06-19 18:32:28 +0200

    Refactor migrations tools (#329)
    # Conflicts:
    #	config/application.rb
    #	lib/tasks/k8s.rake
    #	lib/tasks/migrate.rake
    #	lib/tasks/scaleway.rake

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,15 @@
 
 require_relative "boot"
 
-require "rails/all"
+require "decidim/rails"
+# Add the frameworks used by your app that are not loaded by Decidim.
+require "action_cable/engine"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require_relative "../lib/active_storage/downloadable"
+
+# TODO : add missing dep to decidim-initiatives/lib/decidim/initiatives/engine.rb
+# require "wicked_pdf"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,8 +19,8 @@ Bundler.require(*Rails.groups)
 module DevelopmentApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-    config.time_zone = "Europe/Paris"
+    config.load_defaults 5.2
+    config.time_zone = "Europe/Paris" unless Rails.env.test?
     config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml").to_s]
 
     # This needs to be set for correct images URLs in emails
@@ -21,9 +29,28 @@ module DevelopmentApp
 
     config.backup = config_for(:backup).deep_symbolize_keys
 
+    config.action_dispatch.default_headers = {
+      "X-Frame-Options" => "SAMEORIGIN",
+      "X-XSS-Protection" => "1; mode=block",
+      "X-Content-Type-Options" => "nosniff"
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.to_prepare do
+      ActiveStorage::Blob.include ActiveStorage::Downloadable
+    end
+
+    config.after_initialize do
+      require "extends/controllers/decidim/devise/sessions_controller_extends"
+      require "extends/controllers/decidim/editor_images_controller_extends"
+
+      Decidim::GraphiQL::Rails.config.tap do |config|
+        config.initial_query = "{\n  deployment {\n    version\n    branch\n    remote\n    upToDate\n    currentCommit\n    latestCommit\n    locallyModified\n  }\n}".html_safe
+      end
+    end
   end
 end

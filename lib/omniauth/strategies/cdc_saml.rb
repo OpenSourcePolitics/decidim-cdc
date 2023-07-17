09701cbf0c43db464e1e4cb2002cd7b5f89437ca
moustachu <moustachu@opensourcepolitics.eu>
2023-07-17 12:54:51 +0200

    fix for matching client SAML configuration

--- lib/omniauth/strategies/cdc_saml.rb
+++ lib/omniauth/strategies/cdc_saml.rb
@@ -5,6 +5,14 @@ require "omniauth-saml"
 module OmniAuth
   module Strategies
     class CDCSAML < OmniAuth::Strategies::SAML
+      option :protocol_binding, "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
+      option :name_identifier_format, "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified"
+
+      option :idp_cert_fingerprint_algorithm, XMLSecurity::Document::SHA256
+      option :security,
+             digest_method: XMLSecurity::Document::SHA256,
+             signature_method: XMLSecurity::Document::RSA_SHA256
+
       info do
         found_attributes = options.attribute_statements.map do |key, values|
           attribute = find_attribute_by(values)

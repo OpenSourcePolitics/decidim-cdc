be84d283a6c78bdf306a6ebe10fbc3f594f98037
AyakorK <guillaume.moret@yahoo.com>
2024-07-04 11:33:06 +0200

    fix: Try to fix migrations

--- db/migrate/20180611125005_add_cancel_data_to_sortition.decidim_sortitions.rb
+++ db/migrate/20180611125005_add_cancel_data_to_sortition.decidim_sortitions.rb
@@ -6,7 +6,8 @@ class AddCancelDataToSortition < ActiveRecord::Migration[5.1]
   def change
     add_column :decidim_module_sortitions_sortitions, :cancel_reason, :jsonb
     add_column :decidim_module_sortitions_sortitions, :cancelled_on, :datetime
-    add_column :decidim_module_sortitions_sortitions, :cancelled_by_user_id, :integer
-    add_index :decidim_module_sortitions_sortitions, :cancelled_by_user_id
+    # rubocop:disable Rails/AddColumnIndex
+    add_column :decidim_module_sortitions_sortitions, :cancelled_by_user_id, :integer, index: true
+    # rubocop:enable Rails/AddColumnIndex
   end
 end
